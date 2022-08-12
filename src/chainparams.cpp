d9694e3413e86114144dbb32c335a40220379089
MonkeyD-Core <62377315+MonkeyD-Core@users.noreply.github.com>
2022-08-12 14:58:04 +0700

    Update BIP34Hash Hash

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -69,7 +69,7 @@ public:
         consensus.script_flag_exceptions.emplace( // Taproot exception
             uint256S("0x00"), SCRIPT_VERIFY_P2SH | SCRIPT_VERIFY_WITNESS);
         consensus.BIP34Height = 42470;
-        consensus.BIP34Hash = uint256("0x4f7acb97a3bfff065721260e90cfcc5e48c5009257f07bc4fdf5e00e7ea28c6f");
+        consensus.BIP34Hash = uint256S("0x9185932f37769357e51b3dd2b8562797b5f4e73e8e8be4acb74659d3982c6bb5");
         consensus.BIP65Height = 42470;
         consensus.BIP66Height = 42470;
         consensus.CSVHeight = 0;
